7ad772eafd4a415fdf2c2170e41e81afad14823f
mikuni <39937720+7174952@users.noreply.github.com>
2025-01-21 21:35:12 +0900

    Add files via upload

--- src/amr_ros/resource/make_voice.py
+++ src/amr_ros/resource/make_voice.py
@@ -7,8 +7,8 @@ client = OpenAI()
 client.api_key = os.getenv('OPENAI_API_KEY')
 
 
-speech_file_path = "voice_test" + ".mp3"
-content_text = "これはボイステストシナリオです"
+speech_file_path = "resp_start_to_run_zh" + ".mp3"
+content_text = "出发了"
 
 response = client.audio.speech.create(
     model="tts-1",
