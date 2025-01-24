a4833a3cf0bb5d83bafabbc1a62ec10d7149d205
mikuni <39937720+7174952@users.noreply.github.com>
2025-01-24 20:54:53 +0900

    屋内の説明文内容更新

--- src/amr_ros/resource/OutSide/make_voice.py
+++ src/amr_ros/resource/OutSide/make_voice.py
@@ -7,8 +7,8 @@ client = OpenAI()
 client.api_key = os.getenv('OPENAI_API_KEY')
 
 
-speech_file_path = "chat_introduce_lab_zh" + ".mp3"
-content_text = "这里是实验室测试楼"
+speech_file_path = "obstacle_alert_zh" + ".mp3"
+content_text = "不好意思，可以让我通过一下吗？"
 
 response = client.audio.speech.create(
     model="tts-1",
